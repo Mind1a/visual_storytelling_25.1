88640396fab9acae8e7f113382386ddc41bcd1ff
AchiGel <ar.gelashvili@gmail.com>
2025-07-25 12:54:44 +0400

    feature/landing styles

--- features/landing/Landing.tsx
+++ features/landing/Landing.tsx
@@ -9,11 +9,18 @@ export const Landing = () => {
     router.push("/home"); 
   };
   return (
-    <div className="flex flex-col justify-center items-center gap-12 mt-[67px] mb-[47px] min-h-screen">
-        <h1 className="text-5xl">ვიზუალური თხრობა</h1>
-        <iframe width="798" height="449" src="https://www.youtube.com/embed/fVWUipoWnf8" title="როგორ გაზარდა ფიტნეს ტრენერმა აუდიტორია 100-დან 40 000 ადამიანამდე -  ნინო ჩერქეზიშვილი, ჩერი" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-        <button onClick={handleClick} className="bg-[#F5B3A3] p-2.5 rounded-[10px] w-[185px] text-white cursor-pointer">გაგრძელება</button>
-        </div>
+    <div className="flex flex-col justify-center items-center gap-12 md:mt-[67px] md:mb-[47px] px-[33px] min-h-screen md:min-h-auto">
+        <h1 className="font-[FiraGO-Regular] text-2xl md:text-5xl text-center tracking-[3.36px] md:tracking-[6.72px]">ვიზუალური თხრობა</h1>
+        <iframe 
+        className="w-full md:w-[798px] h-[219px] md:h-[449px]"
+        src="https://www.youtube.com/embed/fVWUipoWnf8" 
+        title="როგორ გაზარდა ფიტნეს ტრენერმა აუდიტორია 100-დან 40 000 ადამიანამდე -  ნინო ჩერქეზიშვილი, ჩერი" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+        </iframe>
+        <button 
+        onClick={handleClick} 
+        className="bg-[#F5B3A3] p-2.5 rounded-[10px] w-full md:w-[185px] max-w-[325px] font-[BPG-Nino-Mtavruli] font-bold text-white cursor-pointer">გაგრძელება</button>
+    </div>
   )
 }
 
